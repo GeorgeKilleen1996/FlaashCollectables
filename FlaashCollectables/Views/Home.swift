17d3e5c661767d48b6adc86f4cf9ea1e74dc9be9
GeorgeKilleen1996 <georgekilleen1996@hotmail.com>
2022-01-10 04:02:05 +0000

    Completely new revamp from the old Flash Collectables... New login/register page. Making use of Views and Controllers. Started the base design of the home page after logging in, added all of the tiles to create a template for where the information is going to go.

--- FlaashCollectables/Views/Home.swift
+++ FlaashCollectables/Views/Home.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 import Firebase
 
+struct Option: Hashable{
+    let title: String
+    let imageName: String
+    let sectionID: Int
+}
+
 struct Home: View {
     
     @AppStorage("log_Status") var status = false
+    
+    
+    //Creating a storage for my navbar items to be stored
+    let options: [Option] = [
+        .init(title: "Inventory", imageName: "folder.circle", sectionID: 1),
+        .init(title: "Card Singles", imageName: "1.circle", sectionID: 1),
+        .init(title: "Card Bundles", imageName: "gift.circle", sectionID: 1),
+        .init(title: "Graded Cards", imageName: "10.circle", sectionID: 1),
+        .init(title: "Sealed Product", imageName: "archivebox.circle", sectionID: 1),
+        .init(title: "Accessories", imageName: "star.circle", sectionID: 1),
+        .init(title: "Financials", imageName: "sterlingsign.circle", sectionID: 2),
+        .init(title: "Sales", imageName: "cart.circle", sectionID: 2),
+        .init(title: "Monthly Figures", imageName: "doc.circle", sectionID: 2),
+        .init(title: "Business Tools", imageName: "link.circle", sectionID: 3),
+        .init(title: "Buying Calculator", imageName: "b.circle", sectionID: 3),
+        .init(title: "Profit Calculator", imageName: "p.circle", sectionID: 3),
+        .init(title: "Account", imageName: "person.circle", sectionID: 4)
+    ]
     
     var body: some View {
         //Setting up the screen size and properties
@@ -19,16 +43,39 @@ struct Home: View {
         HStack(spacing: 0){
             VStack(alignment: .leading, spacing: 18, content: {
                 
-                //Adding the Login View...
-                
-                Text("Flaash \nCollectables")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    
+                //Adding the navbar...
                 Spacer()
-                
+                HStack{
+                    Spacer()
+                    Text("Flaash\nCollectables")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                }
+                Divider()
+                //Spacer()
+                //Displaying my navbar items
+                VStack(alignment: .leading){
+                    ForEach(options, id: \.self){ option in
+                        HStack{
+                            Button(action:{}){
+                            Image(systemName: option.imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30)
+                            
+                            Text(option.title)
+                                    .font(.system(size: 14))
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+                //.padding()
+                //Spacer()
+                Divider()
                 Button(action:{
                     try? Auth.auth().signOut()
                     withAnimation{status = false}
@@ -51,13 +98,113 @@ struct Home: View {
             .background(Color.black)
             .zIndex(1)
             
-            //Image for the login page...
-            Image("Background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: width/1.4)
-                .clipped()
-                .zIndex(0)
+            //Image background for main content...
+            ZStack{
+                Image("Background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: width/1.4)
+                    .clipped()
+                    .zIndex(0)
+                ZStack{
+                    
+                    //Creating the tile to show whos logged in...
+                    ZStack (alignment: .leading){
+                        //The tile
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.black.opacity(0.4))
+                            .frame(width: 200, height: 50)
+                            .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1))
+                            .overlay(
+                                //Adding who's logged in...
+                                Text("George Killeen")
+                                    .padding(.leading, 25))
+                            .padding(10)
+                        
+                        //Tile content...
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 70, height: 40)
+                        //Profile Image...
+                            .overlay(Image(systemName: "person.circle")
+                                        .foregroundColor(.black)
+                                        .font(.system(size: 35)))
+                        
+                        
+                    }
+                    .frame(width: width/1.4, height: getRect().height - 180, alignment:
+                                .topTrailing)
+                    
+                    ZStack{
+                    //Adding the main tile for the home page
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 410, height: getRect().height - 200)
+                            .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1))
+                            .padding(10)
+                            
+                    }
+                    .frame(width: width/1.4, height: getRect().height - 180, alignment:
+                                .topLeading)
+                    
+                    //Adding three subtiles for homepage
+                    
+                    ZStack{
+                    //Sub tile 1
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 200, height: 165)
+                            .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1))
+                            .padding(10)
+                            .padding(.top, 65)
+                            
+                    }
+                    .frame(width: width/1.4, height: getRect().height - 180, alignment:
+                                .topTrailing)
+                    
+                    ZStack{
+                    //Sub tile 2
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 200, height: 165)
+                            .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1))
+                            .padding(10)
+                            .padding(.top, 245)
+                            
+                    }
+                    .frame(width: width/1.4, height: getRect().height - 180, alignment:
+                                .topTrailing)
+                    
+                    ZStack{
+                    //Sub tile 3
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 200, height: 165)
+                            .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1))
+                            .padding(10)
+                            .padding(.top, 425)
+                            
+                    }
+                    .frame(width: width/1.4, height: getRect().height - 180, alignment:
+                                .topTrailing)
+                    
+                    
+                    
+                    
+                }
+                
+            }
+            
         }
         .ignoresSafeArea()
         .frame(width: width, height: getRect().height - 180)
